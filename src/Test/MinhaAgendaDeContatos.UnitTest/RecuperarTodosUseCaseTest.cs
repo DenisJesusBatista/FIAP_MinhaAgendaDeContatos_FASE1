0db6866fd438bbac38aec088f51004ca84145a78
Jessica Uchôa Pinheiro <=>
2024-04-14 19:32:14 -0300

    test: testes Recuperar Todos useCase

---
+++ src/Test/MinhaAgendaDeContatos.UnitTest/RecuperarTodosUseCaseTest.cs
@@ -0,0 +1,43 @@
+﻿using AutoBogus;
+using AutoMapper;
+using Bogus;
+using FluentAssertions;
+using MinhaAgendaDeContatos.Application.UseCases.Contato.RecuperarTodos;
+using MinhaAgendaDeContatos.Comunicacao.Resposta;
+using MinhaAgendaDeContatos.Domain.Entidades;
+using MinhaAgendaDeContatos.Domain.Repositorios;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaAgendaDeContatos.UnitTest
+{
+    public class RecuperarTodosUseCaseTest
+    {
+        private readonly Mock<IContatoReadOnlyRepositorio> _repositorioReadOnly;
+        private readonly Mock<IMapper> _mapper;
+        private readonly IRecuperarTodosContatosUseCase _useCase;
+        public RecuperarTodosUseCaseTest()
+        {
+            _repositorioReadOnly = new Mock<IContatoReadOnlyRepositorio>();
+            _mapper = new Mock<IMapper>();
+            _useCase = new RecuperarTodosContatosUseCase(_repositorioReadOnly.Object, _mapper.Object);
+        }
+
+        [Fact]
+        public async Task Executar_Deve_Retornar_Com_Sucesso_Tipo_Correto()
+        {
+            //Arrange
+            var repositorioResult = new AutoFaker<Contato>().Generate(new Faker().Random.Int(0, 1000));
+            _repositorioReadOnly.Setup(x => x.RecuperarTodosContatos()).ReturnsAsync(repositorioResult);
+            //Act
+            var result = await _useCase.Executar();
+
+            //Assert
+            result.Should().BeAssignableTo<RespostaContatoJson>();
+        }
+    }
+}

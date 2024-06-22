43ba6a5070cd38fb6621a4a73698d697eea119f6
Jessica Uchôa Pinheiro <=>
2024-06-22 03:14:57 -0300

    feat: implementação inicial dos testes de integração. Endpoint criação de contato

---
+++ src/Test/MinhaAgendaDeContatos.IntegrationTest/CustomWebApplicationFactory.cs
@@ -0,0 +1,74 @@
+using DotNet.Testcontainers.Builders;
+using FluentAssertions.Common;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using MinhaAgendaDeContatos.Domain.Repositorios;
+using MinhaAgendaDeContatos.Infraestrutura.AcessoRepositorio;
+using MinhaAgendaDeContatos.Infraestrutura.AcessoRepositorio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testcontainers.PostgreSql;
+using Xunit;
+
+namespace MinhaAgendaDeContatos.IntegrationTest
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+    {
+        public CustomWebApplicationFactory()
+        {
+            _container = new PostgreSqlBuilder()
+                            .WithPortBinding(5432)
+                            .WithDatabase("minhaagenda")
+                            .WithPassword("postgres")
+                            .Build();
+        }
+
+        private readonly PostgreSqlContainer _container;
+
+        public Task InitializeAsync()
+        {
+            return _container.StartAsync();
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(s =>
+            {
+                var descriptorType =
+               typeof(DbContextOptions<MinhaAgendaDeContatosContext>);
+
+                var descriptor = s
+                    .SingleOrDefault(se => se.ServiceType == descriptorType);
+
+                if (descriptor is not null)
+                {
+                    s.Remove(descriptor);
+                }
+
+                s.AddDbContext<MinhaAgendaDeContatosContext>(ctx =>
+                {
+                    ctx.UseNpgsql(_container.GetConnectionString());
+                });
+                s.AddScoped<IContatoWriteOnlyRepositorio, ContatoRepositorio>()
+                .AddScoped<IContatoReadOnlyRepositorio, ContatoRepositorio>()
+                .AddScoped<IDDDRegiao, DDDRegiaoRepositorio>()
+                .AddScoped<IContatoUpdateOnlyRepositorio, ContatoRepositorio>();
+            });
+        }
+
+        Task IAsyncLifetime.DisposeAsync()
+        {
+            return _container.StopAsync();
+        }
+    }
+
+}
---
+++ src/Test/MinhaAgendaDeContatos.IntegrationTest/RegistarContatoTests.cs
@@ -0,0 +1,46 @@
+﻿using AutoBogus;
+using Bogus;
+using Bogus.Extensions;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using MinhaAgendaDeContatos.Api.Response;
+using MinhaAgendaDeContatos.Comunicacao.Requisicoes;
+using MinhaAgendaDeContatos.Infraestrutura.AcessoRepositorio;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MinhaAgendaDeContatos.IntegrationTest
+{
+    public class RegistrarContatoTests : IClassFixture<CustomWebApplicationFactory>
+    {
+        protected readonly HttpClient _client;
+        public RegistrarContatoTests(CustomWebApplicationFactory factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task RegistrarContato_Quando_Sucesso_Deve_Retornar_Ok_Com_Mensagem_Correta()
+        {
+            //Arrange
+            var contato = new AutoFaker<RequisicaoRegistrarContatoJson>()
+                .RuleFor(x => x.TelefoneProxy, 88888888)
+                .RuleFor(x => x.PrefixoProxy, 99)
+                .RuleFor(x => x.Email, new Faker().Person.Email)
+                .Generate();
+
+            StringContent body = new(JsonSerializer.Serialize(contato), Encoding.UTF8, "application/json");
+
+            //Act
+            var response = await _client.PostAsync("https://localhost:7196/api/contato", body);
+
+            //Assert
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            jsonResponse.Should().Be(ResponseMessages.ContatoCriado);
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}

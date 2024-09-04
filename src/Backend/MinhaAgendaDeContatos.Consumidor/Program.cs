54ace92f929480605467830bde960e8e150452ac
Denis de Jesus Batista <denisdjb2010@hotmail.com>
2024-09-03 21:52:41 -0300

    Projeto consumidor

---
+++ src/Backend/MinhaAgendaDeContatos.Consumidor/Program.cs
@@ -0,0 +1,10 @@
+using MinhaAgendaDeContatos.Consumidor;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();

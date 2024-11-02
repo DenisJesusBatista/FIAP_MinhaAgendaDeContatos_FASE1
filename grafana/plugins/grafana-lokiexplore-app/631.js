748d8fca369b2bf89ecbdca089ea546d54e5e78f
Denis de Jesus Batista <denisdjb2010@hotmail.com>
2024-11-02 11:07:40 -0300

    Implementação do kubernetes.

---
+++ grafana/plugins/grafana-lokiexplore-app/631.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgrafana_lokiexplore_app=self.webpackChunkgrafana_lokiexplore_app||[]).push([[631],{9631:(e,a,n)=>{n.r(a),n.d(a,{default:()=>p});var r=n(5959),l=n.n(r);const t=(0,r.lazy)((()=>Promise.all([n.e(308),n.e(698),n.e(475),n.e(594)]).then(n.bind(n,3594)))),s=l().createContext(null);class o extends l().PureComponent{render(){return l().createElement(s.Provider,{value:this.props},l().createElement(t,null))}}const p=o}}]);
+//# sourceMappingURL=631.js.map
